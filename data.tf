b751f23bcf0fb1a359b9b1fad088f1635c08ba03
Hazmei Abdul Rahman <hazmei@sph.com.sg>
2023-12-08 11:32:22 +0800

    Move code block to respective group

--- data.tf
+++ data.tf
@@ -1,2 +1,24 @@
 data "aws_region" "current" {}
+
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "vault_ent_secrets_manager_access" {
+  statement {
+    sid = "VaultEntSecretsManagerAccess"
+    actions = [
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:CreateSecret",
+      "secretsmanager:PutSecretValue",
+      "secretsmanager:UpdateSecret",
+      "secretsmanager:UpdateSecretVersionStage",
+      "secretsmanager:DeleteSecret",
+      "secretsmanager:RestoreSecret",
+      "secretsmanager:TagResource",
+      "secretsmanager:UntagResource",
+    ]
+    resources = [
+      "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:vault/*"
+    ]
+  }
+}
