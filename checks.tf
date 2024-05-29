524fab5741f1cf2b7bea18ae1bfd80ec4c067d78
Hazmei Abdul Rahman <hazmei@sph.com.sg>
2024-05-29 17:09:23 +0800

    chore: update terraform checks description (#8)

--- checks.tf
+++ checks.tf
@@ -3,7 +3,7 @@ check "check_iam_key_age_vault_secretsync" {
     condition = (
       timecmp(coalesce(aws_iam_access_key.vault_secretsync.create_date, local.age_in_days), local.age_in_days) > 0
     )
-    error_message = format("The IAM key for metrics user %s is older than 90 days. Please rotate the key.",
+    error_message = format("The IAM key for metrics user %s is older than 30 days. Please rotate the key.",
     module.iam_user_secretsync.iam_user_name)
   }
 }
